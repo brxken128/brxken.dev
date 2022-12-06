2785213053a4fec26eec2c09364a19f3a1cf5b0c
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-12-06 20:24:45 +0000

    terrible design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			'accent': '#D53C9F',
-		},
-		fontFamily: {
-			sans: ["PT Sans", "sans-serif"],
-		},
-		extend: {},
+		extend: {
+			colors: {
+				// 'accent': '#D53C9F',
+				'accent': '#E9A6A6',
+				'purplevoid': '#1F1D36',
+				'greypurple': '#3F3351',
+				'mutedpurple': '#864879',
+				// 'pinksand': '#E9A6A6'
+			},
+			fontFamily: {
+				sans: ["PT Sans", "sans-serif"],
+			},
+		}
 	},
 	plugins: [],
 }
